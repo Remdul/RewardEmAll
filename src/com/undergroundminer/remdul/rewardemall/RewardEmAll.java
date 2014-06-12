9d56a897855521eec6856daef46735a457acf482
Bryan Elliott <remdul@undergroundminer.com>
2014-06-12 10:47:52 -0400

    Updating Github
    Minecraft 1.7.9 And many changes.

--- src/com/undergroundminer/remdul/rewardemall/RewardEmAll.java
+++ src/com/undergroundminer/remdul/rewardemall/RewardEmAll.java
@@ -1,13 +1,17 @@
 package com.undergroundminer.remdul.rewardemall;
 
+import java.util.logging.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
 import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
-import org.bukkit.Color;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Material;
@@ -18,6 +22,7 @@ import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -30,26 +35,49 @@ public class RewardEmAll extends JavaPlugin implements Listener
 
 	static String maindir = "Plugins/RewardEmAll/";
 	static File config = new File(maindir + "config.yml");
-
+	private static final Logger log = Logger.getLogger("Minecraft");
 	public static Permission permission = null;
 	public static Chat chat = null;
+	public static Economy economy = null;
+	public int checkBit = 0;
 
 	public void onEnable()
 	{
 		getServer().getPluginManager().registerEvents(this, this);
 		setupPermissions();
 		setupChat();
+		if (!setupEconomy())
+		{
+			log.severe(String.format(
+					"[%s] - Disabled due to no Vault dependency found!",
+					getDescription().getName()));
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
 		new File(maindir).mkdir();
 		if (!config.exists())
 		{
 			try
 			{
 				config.createNewFile();
+				this.getConfig().set("ConfigVersion", 1);
+				this.getConfig().set("FireworksTotal", 4);
 				this.getConfig().set("XPTotal", 1000);
 				this.getConfig().set("FoodTotal", 20);
 				this.getConfig().set("HealTotal", 20);
 				this.getConfig().set("CookieTotal", 64);
 				this.getConfig().set("CookieName", "Remdul's Amazing Cookie");
+				this.getConfig().set("msgHeal", "has healed you!");
+				this.getConfig().set("msgFeed", "has fed you!");
+				this.getConfig().set("msgXp", "has given you some experience!");
+				this.getConfig().set("msgFireworks",
+						"celebrates with Fireworks!");
+				this.getConfig().set("msgItem", "has bestowed a gift on you!");
+				this.getConfig().set("msgPay", "has deepened your pocketbook!");
+				this.getConfig().set("msgEffect",
+						"has given you some exciting boosters!");
+				this.getConfig()
+						.set("msgCookie", "has given you some cookies!");
 				this.saveConfig();
 			}
 			catch (IOException e)
@@ -68,6 +96,22 @@ public class RewardEmAll extends JavaPlugin implements Listener
 	public static String translateColor(String input)
 	{
 		return input = ChatColor.translateAlternateColorCodes('&', input);
+	}
+
+	private boolean setupEconomy()
+	{
+		if (getServer().getPluginManager().getPlugin("Vault") == null)
+		{
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer()
+				.getServicesManager().getRegistration(Economy.class);
+		if (rsp == null)
+		{
+			return false;
+		}
+		economy = rsp.getProvider();
+		return economy != null;
 	}
 
 	private boolean setupPermissions()
@@ -89,6 +133,7 @@ public class RewardEmAll extends JavaPlugin implements Listener
 		return chat != null;
 	}
 
+	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args)
 	{
@@ -96,11 +141,45 @@ public class RewardEmAll extends JavaPlugin implements Listener
 		int healtotal = getConfig().getInt("HealTotal");
 		int foodtotal = getConfig().getInt("FoodTotal");
 		int cookietotal = getConfig().getInt("CookieTotal");
+		int fireworkstotal = getConfig().getInt("FireworksTotal");
+		String msgHeal = getConfig().getString("msgHeal");
+		String msgFeed = getConfig().getString("msgFeed");
+		String msgXp = getConfig().getString("msgXp");
+		String msgFireworks = getConfig().getString("msgFireworks");
+		String msgItem = getConfig().getString("msgItem");
+		String msgPay = getConfig().getString("msgPay");
+		String msgEffect = getConfig().getString("msgEffect");
+		String msgCookie = getConfig().getString("msgCookie");
 		Player p = (Player) sender;
 		String cookiename = getConfig().getString("CookieName");
 		String pprefix = chat.getPlayerPrefix(p.getWorld(), p.getName());
 
-		if (args.length > 1)
+		if ((args.length > 1)
+				&& (!(cmd.getName().equalsIgnoreCase("rea") && (args[0]
+						.equalsIgnoreCase("item")
+						|| args[0].equalsIgnoreCase("effect") || args[0]
+							.equalsIgnoreCase("pay")))))
+		{
+			sender.sendMessage("Too many Arguments. Please refer to: /rea help");
+			return true;
+		}
+		if ((args.length > 3)
+				&& ((cmd.getName().equalsIgnoreCase("rea") && (args[0]
+						.equalsIgnoreCase("item")))))
+		{
+			sender.sendMessage("Too many Arguments. Please refer to: /rea help");
+			return true;
+		}
+		if ((args.length > 4)
+				&& ((cmd.getName().equalsIgnoreCase("rea") && (args[0]
+						.equalsIgnoreCase("effect")))))
+		{
+			sender.sendMessage("Too many Arguments. Please refer to: /rea help");
+			return true;
+		}
+		if ((args.length > 2)
+				&& ((cmd.getName().equalsIgnoreCase("rea") && args[0]
+						.equalsIgnoreCase("pay"))))
 		{
 			sender.sendMessage("Too many Arguments. Please refer to: /rea help");
 			return true;
@@ -110,7 +189,29 @@ public class RewardEmAll extends JavaPlugin implements Listener
 			sender.sendMessage("Not Enough Arguments. Please refer to: /rea help");
 			return true;
 		}
-		else if (args.length == 1)
+		else if ((args.length < 3) && cmd.getName().equalsIgnoreCase("rea")
+				&& (args[0].equalsIgnoreCase("item")))
+		{
+			sender.sendMessage("Not Enough Arguments. Please refer to: /rea help");
+			return true;
+		}
+		else if ((args.length < 4) && cmd.getName().equalsIgnoreCase("rea")
+				&& (args[0].equalsIgnoreCase("effect")))
+		{
+			sender.sendMessage("Not Enough Arguments. Please refer to: /rea help");
+			return true;
+		}
+
+		else if ((args.length < 2) && cmd.getName().equalsIgnoreCase("rea")
+				&& args[0].equalsIgnoreCase("pay"))
+		{
+			sender.sendMessage("Not Enough Arguments. Please refer to: /rea help");
+			return true;
+		}
+		else if (args.length == 1
+				|| (args[0].equalsIgnoreCase("item")
+						|| args[0].equalsIgnoreCase("pay") || args[0]
+							.equalsIgnoreCase("effect")))
 		{
 			if (cmd.getName().equalsIgnoreCase("rea"))
 			{
@@ -121,7 +222,7 @@ public class RewardEmAll extends JavaPlugin implements Listener
 					{
 						player.setHealth(healtotal);
 						player.sendMessage(translateColor(pprefix)
-								+ sender.getName() + " has healed you!");
+								+ sender.getName() + " " + msgHeal);
 					}
 					return true;
 				}
@@ -132,7 +233,7 @@ public class RewardEmAll extends JavaPlugin implements Listener
 					{
 						player.setFoodLevel(foodtotal);
 						player.sendMessage(translateColor(pprefix)
-								+ sender.getName() + " has fed you!");
+								+ sender.getName() + " " + msgFeed);
 					}
 					return true;
 				}
@@ -143,21 +244,29 @@ public class RewardEmAll extends JavaPlugin implements Listener
 					{
 						player.giveExp(xptotal);
 						player.sendMessage(translateColor(pprefix)
-								+ sender.getName() + " has given you "
-								+ xptotal + " experience!");
+								+ sender.getName() + " " + msgXp);
 					}
 					return true;
 				}
-				else if (args[0].equalsIgnoreCase("Speed")
-						&& permission.has(sender, "rea.speed"))
+				else if (args[0].equalsIgnoreCase("effect")
+						&& permission.has(sender, "rea.effect"))
 				{
+
+					int dur = Integer.parseInt(args[2]);
+					int amp = Integer.parseInt(args[3]);
+					String effect = args[1];
 					for (Player player : getServer().getOnlinePlayers())
 					{
-						player.addPotionEffect(new PotionEffect(
-								PotionEffectType.SPEED, 2000, 2));
-						player.sendMessage(translateColor(pprefix)
-								+ sender.getName()
-								+ " has given you Speeeeeeeeed!");
+						if (effectsChooser(player, effect, dur, amp) == true)
+						{
+							player.sendMessage(translateColor(pprefix)
+									+ sender.getName() + " " + msgEffect);
+							return true;
+						}
+						else
+						{
+							return true;
+						}
 					}
 					return true;
 				}
@@ -175,13 +284,17 @@ public class RewardEmAll extends JavaPlugin implements Listener
 					sender.sendMessage(ChatColor.DARK_GRAY
 							+ "  /rea xp - Gives everyone some xp");
 					sender.sendMessage(ChatColor.DARK_GRAY
-							+ "  /rea speed - Gives everyone speed");
+							+ "  /rea effect <effect> <duration> <amplitude>");
 					sender.sendMessage(ChatColor.DARK_GRAY
 							+ "  /rea heal - Heals everyone online");
 					sender.sendMessage(ChatColor.DARK_GRAY
 							+ "  /rea feed - Feeds everyone online");
 					sender.sendMessage(ChatColor.DARK_GRAY
 							+ "  /rea fireworks - No better way to celebrate!");
+					sender.sendMessage(ChatColor.DARK_GRAY
+							+ "  /rea item <item> <amount> - Gives everyone X Items");
+					sender.sendMessage(ChatColor.DARK_GRAY
+							+ "  /rea pay <amount> - Gives everyone X Currency");
 					sender.sendMessage(ChatColor.BLUE
 							+ "+----------------------------------------------+");
 					return true;
@@ -198,23 +311,96 @@ public class RewardEmAll extends JavaPlugin implements Listener
 						cookie.setItemMeta(item);
 						player.getInventory().addItem(cookie);
 						player.sendMessage(translateColor(pprefix)
-								+ sender.getName() + " has given you some "
-								+ cookiename + "! So Tasty!");
+								+ sender.getName() + " " + msgCookie);
 					}
 					return true;
+				}
+				else if (args[0].equalsIgnoreCase("Pay")
+						&& permission.has(sender, "rea.pay"))
+				{
+					for (Player player : getServer().getOnlinePlayers())
+					{
+						EconomyResponse r = economy.depositPlayer(
+								player.getName(), Integer.parseInt(args[1]));
+						if (r.transactionSuccess())
+						{
+							sender.sendMessage(String.format(
+									"You have given everyone %s!",
+									economy.format(r.amount)));
+						}
+						else
+						{
+							sender.sendMessage(String.format(
+									"An error occured: %s", r.errorMessage));
+						}
+						player.sendMessage(translateColor(pprefix)
+								+ sender.getName() + " " + msgPay);
+						player.sendMessage("You now have: " + r.balance);
+					}
+					return true;
+				}
+				else if (args[0].equalsIgnoreCase("Item")
+						&& permission.has(sender, "rea.item"))
+				{
+					for (Player player : getServer().getOnlinePlayers())
+					{
+						PlayerInventory inventory = player.getInventory();
+						if (args[1].indexOf(":") != -1)
+						{
+							String itemvalue[] = args[1].split(":");
+							String item = itemvalue[0];
+							String value = itemvalue[1];
+							short valuemod = 0;
+							try
+							{
+								valuemod = Short.parseShort(value);
+							}
+							catch (NumberFormatException e)
+							{
+								sender.sendMessage("Error: Make sure you're using the proper data value!");
+								return true;
+							}
+
+							Material modstack = Material.matchMaterial(item
+									.toUpperCase());
+							ItemStack stack = new ItemStack(modstack);
+							stack.setAmount(Integer.parseInt(args[2]));
+							stack.setDurability(valuemod);
+							inventory.addItem(stack);
+							player.sendMessage(translateColor(pprefix)
+									+ sender.getName() + " " + msgItem);
+							return true;
+						}
+						else
+						{
+							Material newstack = Material.matchMaterial(args[1]
+									.toUpperCase());
+
+							if (newstack == null)
+							{
+								sender.sendMessage("Invalid Item. If item has two words, replace the spaces with _ or use the Item ID.");
+								return false;
+							}
+							ItemStack stack = new ItemStack(newstack);
+							stack.setAmount(Integer.parseInt(args[2]));
+							inventory.addItem(stack);
+							player.sendMessage(translateColor(pprefix)
+									+ sender.getName() + " " + msgItem);
+						}
+						return true;
+					}
 				}
 				else if (args[0].equalsIgnoreCase("Fireworks")
 						&& permission.has(sender, "rea.fireworks"))
 				{
 					for (Player player : getServer().getOnlinePlayers())
 					{
-						launchFireWorks(player);
-						launchFireWorks(player);
-						launchFireWorks(player);
-						launchFireWorks(player);
+						for (int x = fireworkstotal; x > 0; x = x - 1)
+						{
+							launchFireWorks(player);
+						}
 						player.sendMessage(translateColor(pprefix)
-								+ sender.getName()
-								+ " celebrates with Fireworks!");
+								+ sender.getName() + " " + msgFireworks);
 					}
 					return true;
 				}
@@ -223,6 +409,128 @@ public class RewardEmAll extends JavaPlugin implements Listener
 					sender.sendMessage("Not a recognized command. Usage: /rea help");
 				}
 			}
+		}
+
+		return true;
+	}
+
+	public boolean effectsChooser(Player player, String effect, int dur, int amp)
+	{
+		if (effect.equalsIgnoreCase("Absorption"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.ABSORPTION, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("Blindness"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("Confusion"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("Damage_resistance"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.DAMAGE_RESISTANCE, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("FAST_DIGGING"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.FAST_DIGGING, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("FIRE_RESISTANCE"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.FIRE_RESISTANCE, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("HARM"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HARM, dur,
+					amp));
+		}
+		else if (effect.equalsIgnoreCase("HEAL"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, dur,
+					amp));
+		}
+		else if (effect.equalsIgnoreCase("HEALTH_BOOST"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.HEALTH_BOOST, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("HUNGER"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("INCREASE_DAMAGE"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.INCREASE_DAMAGE, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("INVISIBILITY"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.INVISIBILITY, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("JUMP"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, dur,
+					amp));
+		}
+		else if (effect.equalsIgnoreCase("NIGHT_VISION"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.NIGHT_VISION, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("POISON"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.POISON,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("REGENERATION"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.REGENERATION, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("SATURATION"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.SATURATION, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("SLOW"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.SLOW_DIGGING, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("Speed"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("WATER_BREATHING"))
+		{
+			player.addPotionEffect(new PotionEffect(
+					PotionEffectType.WATER_BREATHING, dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("WEAKNESS"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,
+					dur, amp));
+		}
+		else if (effect.equalsIgnoreCase("WITHER"))
+		{
+			player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER,
+					dur, amp));
+		}
+		else
+		{
+			player.sendMessage("Error: Please check your effect. If there's a space, use _.");
+			player.sendMessage("Use one of these Effects:   Absorption, Blindness, Confusion, Damage_resistance, Fast_digging, Fire_resistance, Harm, Heal, Health_boost, Hunger, Increase_Damage, Invisibility, Jump, Night_vision, Poison, Regeneration, Saturation, Slow, Slow_digging, Speed, Water_breathing, Weakness, Wither");
+			return false;
 		}
 		return true;
 	}
